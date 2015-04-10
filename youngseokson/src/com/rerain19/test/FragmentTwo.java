368656b0f7668ecf8c5c37479f8dc923754ec2fb
rerain19 <rerain19@hanmail.net>
2015-04-10 18:04:33 +0900

    로그인후 프래그먼트 페이지 생성

---
+++ youngseokson/src/com/rerain19/test/FragmentTwo.java
@@ -0,0 +1,22 @@
+package com.rerain19.test;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("NewApi")
+public class FragmentTwo extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		//return super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_two, container, false);
+	}
+
+		
+}
--- youngseokson/src/com/rerain19/test/login_ok.java
+++ youngseokson/src/com/rerain19/test/login_ok.java
@@ -1,12 +1,24 @@
 package com.rerain19.test;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.TextureView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class login_ok extends Activity{
 
+	private TextView welcomeUser;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -14,9 +26,46 @@ public class login_ok extends Activity{
 		
 		setContentView(R.layout.login_ok);
 		
-		Toast toast = Toast.makeText(getApplicationContext(), "이동완료", Toast.LENGTH_LONG);
+		welcomeUser = (TextView)findViewById(R.id.welcomeUser);
+		
+		welcomeUser.setText("님 안녕하세요");
+		
+//		Toast toast = Toast.makeText(getApplicationContext(), "이동완료", Toast.LENGTH_LONG);
+//		toast.setGravity(Gravity.CENTER, 0, 0);
+//		toast.show();
+		
+//		Button fragmentOne = (Button)findViewById(R.id.msg);
+//		Button fragmentTwo = (Button)findViewById(R.id.searchList);
+//		
+//		fragmentOne.setOnClickListener((OnClickListener) this);
+	}
+	
+	@SuppressLint("NewApi")
+	public void selectFlag(View view){
+		Toast toast = Toast.makeText(getApplicationContext(), view.toString(), Toast.LENGTH_LONG);
 		toast.setGravity(Gravity.CENTER, 0, 0);
-		toast.show();
+		toast.show();		
+		Fragment fr = null;
+		
+//		switch(view.getId()){
+//			case R.id.msg :
+//				fr = new FragmentOne();
+//				break;
+//			case R.id.searchList:
+//				fr = new FragmentTwo();
+//				break;
+//		}
+		if(view == findViewById(R.id.msg)){
+			fr = new FragmentOne();
+		}
+		if(view == findViewById(R.id.searchList)){
+			fr = new FragmentTwo();
+		}		
+		
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fm.beginTransaction();
+		fragmentTransaction.replace(R.id.fragment_place, fr);
+		fragmentTransaction.commit();
 	}
 
 	
